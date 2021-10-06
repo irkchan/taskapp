7328fecde74cf0fbfeacbd0304ca53077b6a083c
shino8_0921 <ntm10107@gmail.com>
2021-10-06 12:29:35 +0900

    タスク管理アプリの作成

---
+++ taskapp/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 吉田なつみ on 2021/10/05.
+//
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // カテゴリー
+//    @objc dynamic var category = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
